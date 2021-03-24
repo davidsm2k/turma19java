e6f1ff09ce1268f9f92904fe9ef8387dd4da872b
David Santos <davidsm2k@gmail.com>
2021-03-24 16:45:29 -0300

    Primeiro commit turma 19 agora em java

---
+++ Bloco I/Aulas Java/programas-java/AULAS/src/Exemplo/OiMundo.java
@@ -0,0 +1,11 @@
+package Exemplo;
+
+public class OiMundo {
+	
+	public static void main(String[] args) {
+		
+		//escreva
+		System.out.println("Oi turma 19 generation!!!\n");
+		System.out.println("Vamos programar em JAVA!!!!\n");
+	}
+}
