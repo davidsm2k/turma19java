2baab5bb7190e3aa9940638563b2807893a54b7a
David Santos <davidsm2k@gmail.com>
2021-04-07 22:33:26 -0300

    Add alteracoes lista 06 exercicio 03

--- Bloco I/Aulas Java/programas-java/AULAS/src/ExerciciosLista7/ProdutoEletronico.java
+++ Bloco I/Aulas Java/programas-java/AULAS/src/ExerciciosLista7/ProdutoEletronico.java
@@ -4,27 +4,31 @@ public class ProdutoEletronico {
 	private double preco;
 	private String nomeProduto;
 	private String marca;
-	
-	public ProdutoEletronico() {}
-	
+
+	public ProdutoEletronico() {
+	}
+
 	public double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
+
 	public void setNomeProduto(String nomeProduto) {
 		this.nomeProduto = nomeProduto;
 	}
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-	
-	
 }
