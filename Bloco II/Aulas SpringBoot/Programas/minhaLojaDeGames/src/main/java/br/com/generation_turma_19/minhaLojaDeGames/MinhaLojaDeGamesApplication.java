5a9c0884b34c323ce71d2cb6199ac56a6ebbf551
David Santos <davidsm2k@gmail.com>
2021-05-04 19:07:23 -0300

    Add projeto extra 'minhaLojaDeGames'

---
+++ Bloco II/Aulas SpringBoot/Programas/minhaLojaDeGames/src/main/java/br/com/generation_turma_19/minhaLojaDeGames/MinhaLojaDeGamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation_turma_19.minhaLojaDeGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaLojaDeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaLojaDeGamesApplication.class, args);
+	}
+
+}
---
+++ Bloco II/Aulas SpringBoot/Programas/minhaLojaDeGames/src/test/java/br/com/generation_turma_19/minhaLojaDeGames/MinhaLojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation_turma_19.minhaLojaDeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaLojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
