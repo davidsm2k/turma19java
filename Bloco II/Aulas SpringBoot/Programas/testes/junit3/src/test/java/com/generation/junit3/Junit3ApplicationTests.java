44f0363f57ac8c31879c7e4820a4cac770048c29
David Santos <69879401+davidsm2k@users.noreply.github.com>
2021-05-13 08:14:14 -0300

    Add atividades extras sobre testes

---
+++ Bloco II/Aulas SpringBoot/Programas/testes/junit3/src/test/java/com/generation/junit3/Junit3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.junit3;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Bloco II/Aulas SpringBoot/Programas/testes/junit4/src/test/java/com/generation/junit4/Junit4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.junit4;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
